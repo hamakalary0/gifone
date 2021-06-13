e6a3b3fed5bd202d2f4ef2461bd0d890dfcb597f
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-13 03:00:38 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -132,3 +132,49 @@ Revenge.send(new Discord.Attachment(RevengePP))
 //------------------------ HaaaMa Random Gif -----------------------//
 //------------------------ HaaaMa Random Gif -----------------------//
 
+
+
+
+
+client.on("message", vex => { 
+if(vex.content.includes("discord.gg")){ 
+    
+    if (talkedRecently.has(vex.author.id)) {
+      vex.author.send("Wait 1 minute before getting typing this again. - ");
+    } else {
+
+if(vex.channel.type === "dm"){ 
+if(vex.author.id===client.user.id) return; 
+vex.author.send("https://discord.gg/VWuQfQfjc9")
+let s2 = vex .content = vex.content.replace(/@(everyone)/, "🥰").replace(/@(here)/, "🥰");
+let hama1 = client.channels.cache.get("851359393731051550")//id bashe reklam
+hama1.send(`
+ناردرا لە لایەن
+<@${vex.author.id}> ` +`
+سێرڤەرەکە
+${s2}
+          `)
+  }
+
+}}
+  talkedRecently.add(vex.author.id);
+        setTimeout(() => {
+          talkedRecently.delete(vex.author.id);
+        }, 60000);
+  
+  
+})
+
+const channelid = "842770384398188556"    //id channele vc
+client.on("ready", () => {
+    const channel = client.channels.cache.get(channelid);
+    if (!channel) return console.error("The channel does not exist!");
+    channel.join().then(connection => {
+        console.log("Successfully connected.");
+    }).catch(e => {
+        console.error(e);
+    });
+});
+
+
+client.login(config.token)
