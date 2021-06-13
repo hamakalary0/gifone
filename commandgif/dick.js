3453e5ceb3be211d483b45693afdf163e4a3d200
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-14 00:35:35 +0300

    Create dick.js

---
+++ commandgif/dick.js
@@ -0,0 +1,62 @@
+
+const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+exports.run = async (app, message, client) => {
+ 
+
+    //Start
+    message.delete();
+    let sizes = [
+      "8D",
+      "8=D",
+      "8==D",
+      "8===D",
+      "8====D",
+      "8=====D",
+      "8======D",
+      "8=======D",
+      "8========D",
+      "8=========D",
+      "8==========D",
+      "8===========D",
+      "8============D",
+      "8=============D",
+      "8==============D",
+      "8===============D",
+      "8================D",
+      "8==================D"
+    ];
+
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = sizes[Math.floor(Math.random() * sizes.length)];
+
+    let embed = new MessageEmbed()
+      .setColor(`RANDOM`)
+      .setTitle(`Pp v2 Machine`)
+      .setDescription(`${Member.user.username} pp Size Is\n${Result}`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'dicksize',
+  description: 'rexuss',
+  usage: 'dick'
+}; 
