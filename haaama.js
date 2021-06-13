3f973bfad5b6347af8369da015e1a836793067da
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-13 03:10:18 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -132,3 +132,17 @@ Revenge.send(new Discord.Attachment(RevengePP))
 //------------------------ HaaaMa Random Gif -----------------------//
 //------------------------ HaaaMa Random Gif -----------------------//
 
+
+const channelid = "842770384398188556"    //id channele vc
+client.on("ready", () => {
+    const channel = client.channels.cache.get(channelid);
+    if (!channel) return console.error("The channel does not exist!");
+    channel.join().then(connection => {
+        console.log("Successfully connected.");
+    }).catch(e => {
+        console.error(e);
+    });
+});
+
+
+client.login(config.token)
