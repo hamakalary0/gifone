a9d8bdfb2fbd8b22a8ee338164a6b8bc166fd919
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-14 20:06:56 +0300

    Create howgay.js

---
+++ commandgif/howgay.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+exports.run = async (app, message, client, args) => {
+ 
+
+
+    //Start
+   message.delete();
+    let Member =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.member;
+
+    let Result = Math.floor(Math.random() * 101);
+
+    let embed = new MessageEmbed()
+      .setColor(`RANDOM`)
+      .setTitle(`Gay v2 Machine`)
+      .setDescription(`${Member.user.username} Is ${Result}% Gay 🏳️‍🌈`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+   
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'dicksize',
+  description: 'rexuss',
+  usage: 'dick'
+};
