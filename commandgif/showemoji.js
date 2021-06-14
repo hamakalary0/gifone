e091c9e2f0f102d845e20abc4879011a712b8d3c
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-14 21:00:03 +0300

    Update showemoji.js

--- commandgif/showemoji.js
+++ commandgif/showemoji.js
@@ -10,28 +10,7 @@ exports.run = async (app, message, client, args) => {
     const emoji = args[0];
     if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
 
-    let customemoji = Discord.Util.parseEmoji(emoji);
-
-    if (customemoji.id) {
-      const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
-        customemoji.animated ? "gif" : "png"
-      }`;
-      
-      const Added = new MessageEmbed()
-        .setTitle("Emoji Converter")
-        .setColor(`RANDOM`)
-        .addField('<a:waitting:823649927556628481>```Link```', `[Click Me](${Link})`)
-        .setImage(Link
-        );
-      return message.channel.send(Added);
-    } else {
-      let CheckEmoji = (emoji, { assetType: "png" });
-      if (!CheckEmoji[0])
-        return message.channel.send(`Please Give Me A Valid Emoji!`);
-      message.channel.send(
-        `You Can Use Normal Emoji Without Adding In Server!`
-      );
-    }
+   
   }
 
 
