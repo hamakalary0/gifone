878b85ebcf620d3d0a3c112d32f1bdf58ffdc62a
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-14 20:29:16 +0300

    Create rate.js

---
+++ commandgif/rate.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+exports.run = async (app, message, client, args) => {
+ 
+
+
+    //Start
+   
+   let Content = args.join(" ");
+
+    if (!Content)
+      return message.channel.send(`Please Give Me Something To Rate!`);
+
+    let embed = new Discord.MessageEmbed()
+      .setColor(`RANDOM`)
+      .setTitle(`I Rate`)
+      .setDescription(`${Math.floor(Math.random() * 11)}/10 To ${Content}`)
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+}
+
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'rate',
+  description: 'rexuss',
+  usage: 'rate'
+};
