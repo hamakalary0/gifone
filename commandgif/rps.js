1494b28a8d513bead20f4cad4d723949a5a80b50
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-19 14:29:56 +0300

    Update rps.js

--- commandgif/rps.js
+++ commandgif/rps.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 const fs = require("fs");
 const Discord = require("discord.js");
 const chooseArr = ["✌", "✊", "✋"];
