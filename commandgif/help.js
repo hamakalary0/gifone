f763d7ca4f52662409b2cc1f81bbf15d4082cb94
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-19 11:56:49 +0300

    Update help.js

--- commandgif/help.js
+++ commandgif/help.js
@@ -13,31 +13,30 @@ const embed = new Discord.MessageEmbed()
 .setImage(`https://media.discordapp.net/attachments/790836700561670145/838461806137442365/image0.gif`)
 .setAuthor(`RaGif Help`)
 .setDescription(`
-**Info Commands** (*)
-\`server\` - \`about\` - \`user\` - \`say\`
-\`ping\` - \`avatar\` - \`vote\` - \`inv\`
-\`report\` - \`sug\` - \`afk\` - \`invites\`
-\`topinvite\`
+**Info Commands** 
+server  -  about  -  user  -  say
+ping  -  avatar  -  vote  -  inv
+report  -  sug  -  afk  -  invites
+topinvite
 
 **Gifs Commands**
-\`boy\` - \`girl\` - \`baby\`
-\`neon\` - \`sad\` - \`smoking\`
-\`animal\` - \`anime\` - \`couple\`
-\`emoji\` - \`bille\` - \`Aesthetic\`
+boy  -  girl  -  baby
+neon  -  sad  -  smoking
+animal  -  anime  -  couple
+emoji  -  bille  -  Aesthetic
 
 **Photos Command**
-\`Pboy\` - \`Pgirl\` - \`Panime\`
-\`Pneon\` - \`Panimal\` - \`Pcuople\` 
-\`Psmoking\` - \`Pbillie\`
+Pboy  -  Pgirl  -  Panime
+Pneon  -  Panimal  -  Pcuople
+Psmoking  -  Pbillie
 
 **Text Commands**
-\`eng\` - \`arab\` - \`kurd\`
-\`persian\` - \`turk\` - \`rabbit\`
+eng  -  arab  -  kurd
+persian  -  turk  -  rabbit
 
 **memes Command**
-mem - Meme - hmem
-memes - cmeme
-howgay - dicksize
+mem  -  Meme  -  hmem  -  memes  -  cmeme
+howgay  -  dicksize
 
 
 
@@ -50,7 +49,7 @@ message.channel.send({embed});
 exports.conf = {
 enabled: true,
 guildOnly: false,
-aliases: ['help'],
+aliases: ["help", "Help", "HELP"],
 permLevel: 0
 };
 
