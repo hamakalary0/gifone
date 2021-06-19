ad1af417acfa90bfce58f882cdbaec38d7e88724
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-19 21:26:05 +0300

    Update listemoji.js

--- commandgif/listemoji.js
+++ commandgif/listemoji.js
@@ -9,25 +9,28 @@ exports.run = async (app, message, client, args) => {
     let EmojiCount = 0;
     let Animated = 0;
     let OverallEmojis = 0;
-   
+    function Emoji(id) {
+      return client.emojis.cache.get(id).toString();
+    }
     message.guild.emojis.cache.forEach(emoji => {
       OverallEmojis++;
       if (emoji.animated) {
         Animated++;
         EmojisAnimated += Emoji(emoji.id);
-        else {
+      } else {
         EmojiCount++;
         Emojis += Emoji(emoji.id);
       }
-    
-  return message.channel.send(new Discord.MessageEmbed()
+    });
+    let Embed = new Discord.MessageEmbed()
       .setTitle(`Emojis in ${message.guild.name}.`)
       .setDescription(
         `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**`
       )
       .setColor(`RANDOM`);
-    message.channel.send(embed);
+    message.channel.send(Embed);
   }
+
 
 
 
