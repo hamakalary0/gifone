404d6aeb85bdea7dbf15a0939716e88a002c9c45
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-19 21:23:22 +0300

    Update listemoji.js

--- commandgif/listemoji.js
+++ commandgif/listemoji.js
@@ -22,7 +22,7 @@ exports.run = async (app, message, client, args) => {
         Emojis += Emoji(emoji.id);
       }
     
-    let Embed = new Discord.MessageEmbed()
+  return message.channel.send(new Discord.MessageEmbed()
       .setTitle(`Emojis in ${message.guild.name}.`)
       .setDescription(
         `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**`
