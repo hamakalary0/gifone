92a8fef2b970650fc966dfef086aa32ceb3e43fd
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-19 11:38:16 +0300

    Create meme.js

---
+++ commandgif/meme.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+exports.run = async (app, message, client, args) => {
+ 
+
+
+    //Start
+   
+   message.delete();
+    fetch("https://meme-api.herokuapp.com/gimme")
+      .then(res => res.json())
+      .then(json => {
+        let embed = new MessageEmbed()
+          .setColor(`RANDOM`)
+          .setTitle(`${json.title}`)
+          .setURL(json.postLink)
+          .setImage(json.url)
+          .setFooter(`From /r/${json.subreddit}`);
+
+        message.channel.send(embed);
+      });
+
+    //End
+  }
+};
+
+
+
+   
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'meme',
+  description: 'rexuss',
+  usage: 'meme'
+};
