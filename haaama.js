d4e7d2532ed81cb619bafb40205f41d6b4b61b77
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-20 20:28:27 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -511,37 +511,6 @@ client.on("message", message => {
 
 
 
-client.on("message", message => {
-  if (message.content.startsWith(prefix + "listem")) {
-    let Emojis = "";
-    let EmojisAnimated = "";
-    let EmojiCount = 0;
-    let Animated = 0;
-    let OverallEmojis = 0;
-    function Emoji(id) {
-      return client.emojis.cache.get(id).toString();
-    }
-    message.guild.emojis.cache.forEach(emoji => {
-      OverallEmojis++;
-      if (emoji.animated) {
-        Animated++;
-        EmojisAnimated += Emoji(emoji.id);
-      } else {
-        EmojiCount++;
-        Emojis += Emoji(emoji.id);
-      }
-    });
-    let Embed = new Discord.MessageEmbed()
-      .setTitle(`Emojis in ${message.guild.name}.`)
-      .setDescription(
-        `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**`
-      )
-      .setColor(`RANDOM`);
-    message.channel.send(embed);
-  }
-});
-
-
 
 client.login(config.token)
 
