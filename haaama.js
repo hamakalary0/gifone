ca65f8896e5404335d1304687f28a412e23e438d
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-21 00:57:13 +0300

    Update haaama.js

--- haaama.js
+++ haaama.js
@@ -477,47 +477,69 @@ client.on("message", message => {
 ///
 
 
+////
+////
+////
+////
+////
+
+///
+////
+/////
+////
+
+
 client.on("message", message => {
-  if(message.content.startsWith(prefix + "banner")) {
-    if(message.guild.bannerURL() === null || message.guild.bannerURL === undefined) return message.channel.send("**❌ | This server doesn\'t have a banner.**");
-    const ba = new Discord.MessageEmbed()
-    .setAuthor(message.guild.name, message.guild.iconURL())
-    .setDescription(`[Banner URL](${message.guild.bannerURL}?size=2048)`)
-    .setImage(message.guild.bannerURL() + "?size=2048")
-    message.channel.send({embed : ba})
+  if (!message.content.startsWith(prefix)) return;
+  if (!message.channel.guild)
+    return 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  if (command === "kill") {
+    var sabotage = message.mentions.users.first();
+    if (sabotage == message.author)
+      return message.reply(`**No please menition user**`);
+    if (sabotage === client.user) return message.reply(`**Why?**`);
+    if (sabotage < 1) {
+      message.delete();
+      return message.channel.sendMessage(
+        "Put something to kill like mention your username or use an emoji"
+      );
+    }
+    if (!sabotage)
+      return message.channel.send(`Please Mention A Member to Kill :warning:`);
+    message.channel.send("▄︻̷̿┻̿═━一 ${sabotage").then(msg => {
+      msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :three:`);
+      setTimeout(function() {
+        msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :two:`);
+      }, 1000);
+      setTimeout(function() {
+        msg.edit(`▄︻̷̿┻̿═━一 ${sabotage} :one:`);
+      }, 2000);
+      setTimeout(function() {
+        msg.edit(`▄︻̷̿┻̿═━一 :boom:`);
+      }, 3000);
+      setTimeout(function() {
+        msg.edit(`▄︻̷̿┻̿═━一 :fire:`);
+      }, 4000);
+      setTimeout(function() {
+        msg.edit(`▄︻̷̿┻̿═━一 :skull:`);
+      }, 5000);
+      msg.delete(6000);
+      message.delete();
+    });
+    message.channel
+      .send("**** The crime has been successfully hidden 🕳 **")
+      .then(msg => msg.delete(7000));
   }
 });
 
 
 
-////
-////
-////
-////
-////
 
-client.on("message", msg => {
-if (msg.content.startsWith(prefix + "year")){
-    let now = new Date();
-    let next = new Date(now);
-    next.setFullYear(now.getFullYear() + 1);
-    next.setHours(0, 0, 0, 0);
-    next.setMonth(0, 1);
-    let duration = next - now;
-    let seconds = Math.floor((duration / 1000) % 60);
-    let minutes = Math.floor((duration / 1000 / 60) % 60);
-    let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-    let days = Math.floor(duration / (1000 * 60 * 60 * 24));
-    
-    let embed = new Discord.MessageEmbed()
-    .setAuthor("Next Year!", msg.author.displayAvatarURL())
-    .setColor("RANDOM")
-    .setDescription(`There are **${days} days**, **${hours} hours**, **${minutes} minutes** and **${seconds} seconds** until **${next.getFullYear()}**!`)
-    .setImage("")
-    .setFooter(`Or, in short, ${moment.duration(next - now).humanize()}.`)
-    msg.channel.send(embed)
-}
-})
+
+
+
 
 
 
