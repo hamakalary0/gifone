d02087219182b2eb162b51751bdc2b6af8bb739d
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-20 18:43:34 +0300

    Update channel.js

--- commandgif/channel.js
+++ commandgif/channel.js
@@ -10,13 +10,13 @@ const embed = new Discord.MessageEmbed()
 .setColor(`RANDOM`)
 .setFooter(`RaGif Bot`)    
 
-.addField("**ID**", `\`${role.id}\``, true)
-      .addField("**Name**", role.name, true)
-      .addField("**Hex**", role.hexColor)
-      .addField("**Members**", role.members.size)
-      .addField("**Position**", role.position)
-      .addField("**Mentionable**", status[role.mentionable])
-      .addField("**Time Create**", role.createdAt.toLocaleString(), true)
+.addField(**ID**, `\`${role.id}\``, true)
+      .addField(**Name**, role.name, true)
+      .addField(**Hex**, role.hexColor)
+      .addField(**Members**, role.members.size)
+      .addField(**Position**, role.position)
+      .addField(**Mentionable**, status[role.mentionable])
+      .addField(**Time Create**, role.createdAt.toLocaleString(), true)
 
 
 message.channel.send({embed});
