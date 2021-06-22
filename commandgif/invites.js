2ac8babd736519a7b7335ca2aa860ead5986d2b5
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-22 15:09:44 +0300

    Update invites.js

--- commandgif/invites.js
+++ commandgif/invites.js
@@ -8,19 +8,19 @@ exports.run = async (app, message, client, args) => {
 
 
         try {
-    ///        let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+            let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
 
-    ///       let invites = await message.guild.fetchInvites()
+           let invites = await message.guild.fetchInvites()
 
-    ///        let memberInvites = invites.filter(i => i.inviter && i.inviter.id === member.user.id);
+            let memberInvites = invites.filter(i => i.inviter && i.inviter.id === member.user.id);
 
-  //          if (memberInvites.size <= 0) {
-  //              return message.channel.send(`**${member.displayName} didn't invite anyone to the server!**`, (member === message.member ? null : member));
-    // {}          }
+            if (memberInvites.size <= 0) {
+                return message.channel.send(`**${member.displayName} didn't invite anyone to the server!**`, (member === message.member ? null : member));
+     {}          }
 
-     ///     let content = memberInvites.map(i => i.code).join("\n");
-   ///         let index = 0;
-   ///         memberInvites.forEach(invite => index += invite.uses);
+          let content = memberInvites.map(i => i.code).join("\n");
+            let index = 0;
+            memberInvites.forEach(invite => index += invite.uses);
 
             let embed = new Discord.MessageEmbed()
                 .setColor("GREEN")
