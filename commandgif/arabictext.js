1a29f7f1c4e2c40599e8059b773cb4616a15b01f
HaaaMa <80664368+HaaaMa@users.noreply.github.com>
2021-06-24 00:31:22 +0300

    Update arabictext.js

--- commandgif/arabictext.js
+++ commandgif/arabictext.js
@@ -1,14 +1,8 @@
 
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const { MessageEmbed } = require("discord.js");
-const { Prefix } = require("../../config.js");
+exports.run = async (app, message, client, args) => {
 
-module.exports = {
-  name: "textarabic",
-  aliases: ["textArabic"],
-  description: "Show text",
-  usage: "text",
-  run: async (client, message, args) => {
     
     
     let replies = ["هيه حظوظ والطيب يرد خسران من ذيچ الاحلام الچان بانيها:herb:","إذا زارتک شدّه فاعلم أنّها سحابه صیف عن قلیل تقشع، ولا یخیفک رعدها، ولا یرهبک برقها، فربّما کانت محمّلهً بالغیث","چنت اعاتب بالوكت عرفني بيك :broken_heart: :exclamation:","أخاف أفگد طعم باچر يالمتانيك:wilted_rose:","اسألني واني اختصر لعيونك المده :wilted_rose:","وأستحيت من الله حيل ، وآنه سآجد مر ذكرهم والتهيت :wilted_rose:","ما خربتني وياك غير اليكولون:wilted_rose:","شگلّك يالعلي تسأل.. انا التيهت كلشي وكلشي ما اندل:wilted_rose:","هي مسؤولية من تحبلك انسان :broken_heart: "];
@@ -27,3 +21,21 @@ module.exports = {
           });
       }
 }
+
+
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ta", "textarabic"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'textarabaic',
+  description: 'rexuss',
+  usage: 'tarabix'
+};
+
+//txnoembed
